1e01db75cadf1888246acf84a81f83496c354d1c
Troy Harshberger <tharshberger@vendorassistance.com>
2024-02-09 15:12:09 -0700

    Add ironscales extension configuration options

--- src/_nav.jsx
+++ src/_nav.jsx
@@ -708,24 +708,6 @@ const _nav = [
   },
   {
     component: CNavTitle,
-    name: 'MSP Integrations',
-  },
-  {
-    component: CNavGroup,
-    section: 'PSA',
-    name: 'PSA',
-    to: '/msptools/psa',
-    icon: <FontAwesomeIcon icon={faChartBar} className="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'PSA Tickets',
-        to: '/msptools/psa-tickets',
-      },
-    ],
-  },
-  {
-    component: CNavTitle,
     name: 'Settings',
   },
   {
